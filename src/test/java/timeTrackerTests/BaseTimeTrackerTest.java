589294517961218916bf1a47b777ca60e52b9039
ivanpetrovic89 <ivanpetrovic89@gmail.com>
2023-02-05 11:35:27 +0100

    Removed AfterMethod in order to use different AfterMethods in individual tests.

--- src/test/java/timeTrackerTests/BaseTimeTrackerTest.java
+++ src/test/java/timeTrackerTests/BaseTimeTrackerTest.java
@@ -2,8 +2,6 @@ package timeTrackerTests;
 
 import common.Browsers;
 import methods.LoginMethods;
-import methods.MainMethods;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import utilities.PropertyManager;
@@ -21,17 +19,6 @@ public class BaseTimeTrackerTest extends Browsers {
         driver.get(PropertyManager.getInstance().getStartUrl());
         LoginMethods loginMethods = new LoginMethods(driver);
         loginMethods.performLogin(PropertyManager.getInstance().getValidEmail(), PropertyManager.getInstance().getValidPassword());
-    }
-
-    @AfterMethod
-    public void cleanUp(){
-        MainMethods mainMethods = new MainMethods(driver);
-        mainMethods.performCleanUp();
-        tearDown();
-    }
-
-    public void tearDown(){
-        driver.quit();
     }
 
 }
